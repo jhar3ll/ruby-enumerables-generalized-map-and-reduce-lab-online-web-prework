660e698c967838734c367a62d14562c3678344f8
jharell chambers <jharell.chambers@selu.edu>
2019-08-04 22:01:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
-# Your Code Here
+def map(array)
+  new_arr = []
+
+  i = 0
+   while i < array.length do
+    new_arr << yield ( array[i] * -1)
+    
+    i += 1
+  end
+
+ return new_arr  
+end
+ 
+map([1,2,3,-9])
